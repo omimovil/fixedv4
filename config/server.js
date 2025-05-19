9d3d0063210ce1c67a896c6d67eb06c03ac45dce
omimovil <omimovil@gmail.com>
2025-05-19 17:05:43 -0400

    fix: Simplificar configuración de URLs para Railway

--- config/server.js
+++ config/server.js
@@ -9,10 +9,10 @@ module.exports = ({ env }) => ({
   responseTimeout: env.int('SERVER_RESPONSE_TIMEOUT', 100000),
   port: env.int('PORT', 1337),
   host: env('HOST', '0.0.0.0'),
-  url: env('PUBLIC_URL', env('RAILWAY_STATIC_URL') || "https://fixedv4-production-16eb.up.railway.app"),
+  url: env('PUBLIC_URL', env('RAILWAY_STATIC_URL') || "http://0.0.0.0:" + env.int('PORT', 1337)),
   admin: {
     path: env('ADMIN_PATH', '/admin'),
-    url: env('PUBLIC_URL', env('RAILWAY_STATIC_URL') || "https://fixedv4-production-16eb.up.railway.app"),
+    url: env('PUBLIC_URL', env('RAILWAY_STATIC_URL') || "http://0.0.0.0:" + env.int('PORT', 1337)),
   },
   app: {
     keys: env.array('APP_KEYS'),
