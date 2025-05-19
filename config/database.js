e4eb7d3de2a09e5e1043021fe3a375aa2da1ced1
omimovil <omimovil@gmail.com>
2025-05-19 15:19:31 -0400

    fix: Configurar variables de PostgreSQL para Railway

--- config/database.js
+++ config/database.js
@@ -66,17 +66,25 @@ module.exports = ({ env }) => {
     postgres: {
       connection: {
         connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', true) ? {
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false), // Railway suele necesitar false
-        } : false,
+        host: env('PGHOST'),
+        port: env.int('PGPORT', 5432),
+        database: env('PGDATABASE', 'strapi'),
+        user: env('PGUSER', 'strapi'),
+        password: env('PGPASSWORD'),
+        ssl: {
+          rejectUnauthorized: false
+        },
         schema: env('DATABASE_SCHEMA', 'public'),
       },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+      pool: {
+        min: env.int('DATABASE_POOL_MIN', 2),
+        max: env.int('DATABASE_POOL_MAX', 5),
+        createTimeoutMillis: 30000,
+        acquireTimeoutMillis: 30000,
+        idleTimeoutMillis: 30000,
+        reapIntervalMillis: 1000,
+        createRetryIntervalMillis: 100,
+      },
     },
     sqlite: {
       connection: {
