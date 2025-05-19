c56014dd0362073f61252a2481a305462e49ee25
omimovil <omimovil@gmail.com>
2025-05-19 16:13:41 -0400

    fix: Actualizar URL de producción para Railway

--- config/server.js
+++ config/server.js
@@ -9,10 +9,10 @@ module.exports = ({ env }) => ({
   responseTimeout: env.int('SERVER_RESPONSE_TIMEOUT', 100000),
   port: env.int('PORT', 1337),
   host: env('HOST', '0.0.0.0'),
-  url: env('APP_URL', 'https://fixedv4-production-16eb.up.railway.app'),
+  url: env('PUBLIC_URL', env('RAILWAY_STATIC_URL') || "https://fixedv4-production-16eb.up.railway.app"),
   admin: {
     path: env('ADMIN_PATH', '/admin'),
-    url: env('APP_URL', 'https://fixedv4-production-16eb.up.railway.app'),
+    url: env('PUBLIC_URL', env('RAILWAY_STATIC_URL') || "https://fixedv4-production-16eb.up.railway.app"),
   },
   app: {
     keys: env.array('APP_KEYS'),
