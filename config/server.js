aae9b79949c4e562e22a84c151b9ecf701c5f0cf
omimovil <omimovil@gmail.com>
2025-05-19 20:16:37 -0400

    fix: Simplificar configuración para que Railway maneje el proxy y la URL

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('RAILWAY_STATIC_URL'),
 
   app: {
     keys: env.array('APP_KEYS'),
@@ -9,12 +8,5 @@ module.exports = ({ env }) => ({
 
   admin: {
     path: '/admin'
-  },
-
-  settings: {
-    proxy: true,
-    public: {
-      path: '/'
-    }
   }
 });
